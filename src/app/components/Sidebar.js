b3185097eb4ab00965ad0f7f34fdc7d692a2fb50
Aaron Chacon <aaronchacon86@gmail.com>
2021-08-17 18:45:01 -0400

    [feature] - sidebar

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import styled  from 'styled-components';
 
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
+import { SidebarOption } from './SidebarOption';
+
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 
 const SidebarContainer = styled.div`
@@ -12,6 +24,12 @@ const SidebarContainer = styled.div`
     border-top: 1px solid var(--slack-light-color);
     max-width: 260px;
     margin-top: 60px;
+
+    > hr {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #49274b;
+    }
 
 `;
 
@@ -37,6 +55,23 @@ const SidebarInfo = styled.div`
         font-weight: 900;
         margin-bottom: 5px;
     }
+
+    > h3 {
+        display: flex;
+        font-size: 13px;
+        font-weight: 400;
+        align-items: center;
+    }
+
+    > h3 > .MuiSvgIcon-root {
+        font-size: 14px;
+        margin-top: 1px;
+        margin-right: 2px;
+        color: green;
+    }
+
+
+
 `;
 
 export const Sidebar = () => {
@@ -52,6 +87,22 @@ export const Sidebar = () => {
                 </SidebarInfo>
                 <CreateIcon />
             </SidebarHeader>
+
+            <SidebarOption Icon={InsertCommentIcon}  title="Threads" />
+            <SidebarOption Icon={InboxIcon}  title="Mentions & reactions" />
+            <SidebarOption Icon={DraftsIcon}  title="Saved items" />
+            <SidebarOption Icon={BookmarkBorderIcon}  title="Chaneel browser" />
+            <SidebarOption Icon={PeopleAltIcon}  title="People & user groups" />
+            <SidebarOption Icon={AppsIcon}  title="Apps" />
+            <SidebarOption Icon={FileCopyIcon}  title="File browser" />
+            <SidebarOption Icon={ExpandLessIcon}  title="Show less" />
+
+            <hr/>
+            <SidebarOption Icon={ExpandMoreIcon}  title="Channels" />
+            <hr/>
+            <SidebarOption Icon={AddIcon} addChannelOption title="Channels" />
+
+
         </SidebarContainer>
     )
 }
